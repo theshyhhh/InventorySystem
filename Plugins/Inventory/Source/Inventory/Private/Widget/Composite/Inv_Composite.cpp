1edd97afeab827b3f9392eed28d80d6bce6dacb3
Westbrook <504899804@qq.com>
2026-05-07 11:07:41 +0800

    添加Inv_Composite类及其相关部件，支持复合

---
+++ Plugins/Inventory/Source/Inventory/Private/Widget/Composite/Inv_Composite.cpp
@@ -0,0 +1,27 @@
+﻿#include "Widget/Composite/Inv_Composite.h"
+
+#include "Blueprint/WidgetTree.h"
+
+void UInv_Composite::ApplyFunction(FuncType Func)
+{
+	Super::ApplyFunction(Func);
+
+	for (UInv_CompositeBase* Child : Children)
+	{
+		Child->ApplyFunction(Func);
+	}
+}
+
+void UInv_Composite::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	WidgetTree->ForEachWidget([this](UWidget* Widget)
+	{
+		if (UInv_CompositeBase* CompositeBase = Cast<UInv_CompositeBase>(Widget))
+		{
+			Children.Add(CompositeBase);
+			CompositeBase->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	});
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Widget/Composite/Inv_Composite.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Inv_CompositeBase.h"
+#include "Inv_Composite.generated.h"
+
+
+UCLASS()
+class INVENTORY_API UInv_Composite : public UInv_CompositeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ApplyFunction(FuncType Func) override;
+
+protected:
+	virtual void NativeOnInitialized() override;
+
+private:
+	UPROPERTY()
+	TArray<TObjectPtr<UInv_CompositeBase>> Children;
+};
